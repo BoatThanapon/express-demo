753d3164741d002da81eb78e648799673978c0b3
BoatThanapon <Thanapon_chap@hotmail.com>
2018-05-09 23:20:26 +0700

    Test first commit

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index',{title: 'My Express App', message:'Hello world'})
-    res.send('Hello World!!');
+    res.send('Hello World!');
 });
 
 module.exports = router;
